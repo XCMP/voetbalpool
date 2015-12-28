02cb94ab9f47f74963857c18f5167e5577e2a2ce
Marco Prins <marco.prins@icloud.com>
2015-12-28 01:17:31 +0100

    - added id to models - added selecting and deselecting pool player - added buttons (css) - some refactoring of the poolplayers_view

--- src/app/views/poolplayers_view.js
+++ src/app/views/poolplayers_view.js
@@ -14,29 +14,55 @@
 
     initialize: function() {
       this.collection.on('sync', this.render, this);
-      // this.collection.on('add', this.render, this);
       this.collection.fetch();
     },
 
     selectPoolPlayer: function(ev) {
-      this.removeSelection();
-      var $el = $(ev.currentTarget);
-      $el.addClass('selected');
-      this.$selectedPlayer = $el;
+      // TODO refactor???
+      var $clickedPlayer = $(ev.currentTarget);
+
+      if (this.$selectedPlayer === null) {
+        this.setSelectedPoolPlayer($clickedPlayer);
+        this.setButtons();
+        return;
+      }
+
+      if ($clickedPlayer.data('id') === this.$selectedPlayer.data('id')) {
+        this.removeSelectedPoolPlayer();
+        this.setButtons();
+        return;
+      }
+    
+      this.removeSelectedPoolPlayer();
+      this.setSelectedPoolPlayer($clickedPlayer);
+      this.setButtons();
     },
 
-    removeSelection: function() {
+    removeSelectedPoolPlayer: function() {
       if (this.$selectedPlayer) {
         this.$selectedPlayer.removeClass('selected');
         this.$selectedPlayer = null;
       }
     },
 
+    setSelectedPoolPlayer: function($clickedPlayer) {
+        $clickedPlayer.addClass('selected');
+        this.$selectedPlayer = $clickedPlayer;
+    },
+
     render: function() {
       this.$el.html(this.template({
-        poolplayers: this.collection.toJSON()
+        poolplayers: this.collection.toJSON(),
+        active: this.$selectedPlayer === null? 'disabled':''
       }));
       return this;
+    },
+
+    setButtons: function() {
+      var self = this;
+      $(['.js_button_update', '.js_button_delete']).each(function(i, selector) {
+        $(selector).prop("disabled", self.$selectedPlayer === null? true:false);
+      });
     }
 
   });
