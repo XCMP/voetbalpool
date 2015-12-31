01df780055d8741af13812f7c615ecefe371a964
Marco Prins <marco.prins@icloud.com>
2015-12-31 11:04:36 +0100

    - added active menu item styling

--- src/app/views/menu.js
+++ src/app/views/menu.js
@@ -5,16 +5,33 @@
     el: '.menu',
     router: null,
     events: {
-      'click li.poolplayer' : 'showPoolPlayers'
+      'click li.poolplayer' : 'showPoolPlayers',
+      'click li'            : 'setMenuItemActive_temp'
     },
 
     initialize: function(options) {
       this.router = options.router
     },
 
-    showPoolPlayers: function() {
+    showPoolPlayers: function(ev) {
+      this.setMenuItemActive($(ev.currentTarget));
       this.router.navigate('list/poolplayers',  {trigger: true});
     },
+
+    setMenuItemActive_temp: function(ev) {
+      this.setMenuItemActive($(ev.currentTarget));
+    },
+
+    setMenuItemActive: function($el) {
+      this.removeMenuItemActive();
+      $el.addClass('active');
+    },
+
+    removeMenuItemActive: function() {
+      $('li').each(function(i, el) {
+        $(el).removeClass('active');
+      });
+    }
 
   });
 
