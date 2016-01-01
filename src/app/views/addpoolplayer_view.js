dc1b43459683313a1fff06de4767546cab08c0be
Marco Prins <marco.prins@icloud.com>
2016-01-01 03:38:32 +0100

    - implemented model save with success and error handling (work in progress, back-end needed)

--- src/app/views/addpoolplayer_view.js
+++ src/app/views/addpoolplayer_view.js
@@ -13,12 +13,28 @@
     initialize: function() {
       // this.listenTo(this.model, 'change', this.render);
       // this.listenTo(this.model, 'destroy', this.remove);
+      // this.listenTo(this.model, 'error', this.saveError);
     },
 
     saveAddPoolPlayer: function(ev) {
       ev.preventDefault();
       var formData = _utils.formDataToJSON($(ev.currentTarget));
-      this.model.save(formData);
+      var options = {
+        success: function(object, model, options) {
+          console.log('success');
+          console.log('object', object);
+          console.log('model', model);
+          console.log('options', options);
+        },
+        error: function(object, response, options) {
+          console.log('error');
+          console.log('object', object);
+          console.log('response', response);
+          console.log('options', options);
+        },
+      };
+      var result = this.model.save(formData, options);
+      console.log('xcmp ', result);
     },
 
     toPoolPlayerList: function() {
