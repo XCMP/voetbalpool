c7ffd0cab7a9505cdbf1faf5dad7393b9e42a42d
Marco Prins <marco.prins@icloud.com>
2016-02-03 21:42:45 +0100

    - added method to set e chosen period

--- src/app/collections/months.js
+++ src/app/collections/months.js
@@ -1,7 +1,17 @@
+(function(_utils) {
+
   VP.Collections.Months = Backbone.Collection.extend({
 
     model: VP.Models.Month,
 
-    url: 'http://localhost:3001/vp/prediction/months'
+    url: 'http://localhost:3001/vp/prediction/months',
+
+    setPeriod: function(periodString) {
+      _.each(this.models, function(model) {
+        model.set('selected', model.get('yyyymm') === periodString);
+      });
+    }
 
   });
+
+})(VP.utils);
