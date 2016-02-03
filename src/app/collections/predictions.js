bd4b0d57c3f5e9381e9414c98ec0623fb74de46e
Marco Prins <marco.prins@icloud.com>
2016-02-03 21:45:07 +0100

    - fetching extended to use a specific period

--- src/app/collections/predictions.js
+++ src/app/collections/predictions.js
@@ -1,7 +1,21 @@
+(function(_utils) {
+
   VP.Collections.Predictions = Backbone.Collection.extend({
 
     model: VP.Models.Prediction,
 
-    url: 'http://localhost:3001/vp/predictions'
+    url: function() {
+      return 'http://localhost:3001/vp/predictions/' + this.period.year + '/' + this.period.month;
+    },
+
+    initialize: function() {
+      this.period = _utils.getCurrentPeriod();
+    },
+
+    setPeriod: function(periodString) {
+      this.period = _utils.getPeriod(periodString);
+    }
 
   });
+
+})(VP.utils);
