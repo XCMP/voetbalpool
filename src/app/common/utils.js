d060308fb5a7a89093b774a6b456458f9eee943d
Marco Prins <marco.prins@icloud.com>
2016-02-21 17:22:10 +0100

    - removed commented line

--- src/app/common/utils.js
+++ src/app/common/utils.js
@@ -149,7 +149,6 @@ VP.utils = {
 
   getFieldContainer: function(fieldName) {
     return $('[field*=\'' + fieldName + '\']');
-    // return $('[field=\'' + fieldName + '\']');
   },
 
   lineColorClasses: [
