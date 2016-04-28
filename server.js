abba9ed2111482ef3e52dd351c08df11d4c5667b
Marco Prins <marco.prins@icloud.com>
2016-04-28 22:43:23 +0200

    - added delete method to remove incorrect logo files

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 
   var PORT = process.env.OPENSHIFT_NODEJS_PORT || 3000;
   var IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+  var LOGO_DESTINATION_DIR = 'dist/images/logos/';
 
-  var upload = multer({ dest: 'dist/images/logos/' });
+  var upload = multer({ dest: LOGO_DESTINATION_DIR });
   var server = express();
   var router = express.Router();
 
@@ -17,13 +18,13 @@
   server.use(express.static('dist'));
 
   // index.html of voetbalpool app
-  router.get("/",function(req,res){
+  router.get('/',function(req,res){
       res.send(path.join(__dirname + '/index.html'));
   });
 
   // LOGOS
   // get list of logo files
-  router.get("/logos/index.html", function(req,res){
+  router.get('/logos/index.html', function(req,res){
     fs.readdir('dist/images/logos', function (err, files) {
       if (err) {
           throw err;
@@ -38,12 +39,12 @@
   });
 
   // upload logo page
-  router.get("/upload/logo/index.html", function(req,res){
+  router.get('/upload/logo/index.html', function(req,res){
       res.send(uploadMenu + uploadTemplate);
   });
 
   // upload logo end point
-  router.post("/upload/logo", upload.array('logos', 50), function(req,res,next){
+  router.post('/upload/logo', upload.array('logos', 50), function(req,res,next){
     var result = '<ul>';
     req.files.forEach(function(e, i, arr) {
       result += '<li>renamed from ' + e.path + ' to ' + e.destination + e.originalname + '</li>';
@@ -55,6 +56,25 @@
     });
     result += '</ul>';
     res.send(uploadMenu + result);
+  });
+
+  // delete logo image
+  router.delete('/upload/logo/index.html', function(req,res){
+    if (req.query.logoFileName.indexOf('..') >= 0) {
+      res.status(200).send('OK.');
+      return;
+    }
+    var fileToRemove = LOGO_DESTINATION_DIR + req.query.logoFileName;
+    fs.exists(fileToRemove, (exists) => {
+      if (exists) {
+        fs.unlink(fileToRemove, (err) => {
+          if (err) throw err;
+        });
+        res.send('OK. File ' + fileToRemove + ' is deleted.');
+      } else {
+        res.status(404).send('Not found');
+      }
+    });
   });
 
   server.use('/',router);
