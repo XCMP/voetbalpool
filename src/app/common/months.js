f2cb100f02ac4929a371292e66b0519be2f1389f
Marco Prins <marco.prins@icloud.com>
2016-07-11 21:05:50 +0200

    - months select also show 2 months in future

--- src/app/common/months.js
+++ src/app/common/months.js
@@ -4,23 +4,7 @@ VP.Data.months = {
   months: null,
 
   init: function() {
-    var date = moment();
-    const currentPeriod = date.format('YYYY-MM');
-    const lastMonth = this.getLastMonth();
-    const monthsData = [];
-    do {
-      const yearMonthString = date.format('YYYY-MM');
-      const yearMonthObject = this.getPeriod(yearMonthString);
-      monthsData.push({
-        'yyyymm' : yearMonthString,
-        'year': yearMonthObject.year,
-        'month': yearMonthObject.month,
-        'selected': yearMonthString === currentPeriod,
-        'text' : this.getMonthYearText(yearMonthString)
-      });
-      date = this.subtractMonth(date);
-    } while (lastMonth.diff(date) < 0);
-    this.months = monthsData;
+    this.months = this.getMonthsObjectArray();
   },
 
   setSelectedPeriod: function(periodAsString) {
@@ -39,6 +23,30 @@ VP.Data.months = {
     return this.months;
   },
 
+  getLastMonth: function() {
+    return moment('2015-12', 'YYYY-MM');
+  },
+
+  getMonthsObjectArray: function() {
+    const currentPeriod = this.getCurrentYearMonth();
+    var date = this.getStartDate();
+    const lastMonth = this.getLastMonth();
+    const monthsData = [];
+    do {
+      const yearMonthString = date.format('YYYY-MM');
+      const yearMonthObject = this.getPeriod(yearMonthString);
+      monthsData.push({
+        'yyyymm' : yearMonthString,
+        'year': yearMonthObject.year,
+        'month': yearMonthObject.month,
+        'selected': yearMonthString === currentPeriod,
+        'text' : this.getMonthYearText(yearMonthString)
+      });
+      date = this.subtractMonth(date);
+    } while (lastMonth.diff(date) < 0);
+    return monthsData;
+  },
+
   getPeriod: function(periodString) {
     var period = {};
     var periodArray = periodString.split('-');
@@ -47,19 +55,23 @@ VP.Data.months = {
     return period;
   },
 
-  getLastMonth: function() {
-    return moment('2015-12', 'YYYY-MM');
+  getMonthYearText: function(yyyymm) {
+    var yearMonthArray = yyyymm.split('-');
+    var year = yearMonthArray[0];
+    var monthIndex = parseInt(yearMonthArray[1], 10) - 1;
+    return this.MONTH_TEXT_ARRAY[monthIndex] + ' ' + year;
+  },
+
+  getCurrentYearMonth: function () {
+    return moment().format('YYYY-MM');
   },
 
   subtractMonth: function(date) {
     return date.add(-1 , 'M');
   },
 
-  getMonthYearText: function(yyyymm) {
-    var yearMonthArray = yyyymm.split('-');
-    var year = yearMonthArray[0];
-    var monthIndex = parseInt(yearMonthArray[1], 10) - 1;
-    return this.MONTH_TEXT_ARRAY[monthIndex] + ' ' + year;
+  getStartDate: function () {
+    return moment().add(+2, 'M');
   }
 
 };
